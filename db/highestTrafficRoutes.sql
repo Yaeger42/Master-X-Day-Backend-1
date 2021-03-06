992558918cc8056a373115793d9a963bd30cda63
Diego Duarte <projectdcd@gmail.com>
2021-03-06 12:49:38 -0600

    highestTrafficRoutes.sql added, gitignore as well

--- db/highestTrafficRoutes.sql
+++ db/highestTrafficRoutes.sql
@@ -1,9 +1,8 @@
 -- AeroPlatzi's routes and times with the highest traffic.
 
-SELECT 
-    ID --flightId
-
-
-
-FROM flights fl 
-JOIN 
+SELECT
+    *
+FROM flights
+WHERE flightstatusid = 1
+AND departuretime BETWEEN now()-interval '24 hours' AND arrivalTime is null
+GROUP BY originid, destinationid
