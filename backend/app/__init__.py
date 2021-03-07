b4b52db50813983926e0d25669421b1b86b35099
Diego Duarte <projectdcd@gmail.com>
2021-03-06 18:32:22 -0600

    endpoints added in here

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,6 +14,9 @@ def create_app():
     }
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % postgres
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:password@172.17.0.1:5432/flightsDatabase'
+    #'postgresql://root:\%(password)s@%(host)s:%(port)s/%(db)s' % postgres
+    # postgresql://scott:tiger@localhost:5432/mydatabase
+    # dialect+driver://username:password@host:port/database
     print(app.config['SQLALCHEMY_DATABASE_URI'])
     return app 
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import request
 from app import create_app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 from flask_migrate import Migrate
 #from app.models import flightModel
 #from app.config.db import Db
@@ -27,14 +28,76 @@ class FlightStatusModel(db.Model):
         }
 
 
+class BoardingGatesModel(db.Model):
+    __tablename__ = 'boardinggates'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String())
 
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return {
+            "id": self.id,
+            "name" : self.name
+        }
+
+
+
+class FlightsModel(db.Model):
+
+    __tablename__ = 'flights'
+    id = db.Column(db.Integer, primary_key=True)
+    destinationid = db.Column(db.Integer, ForeignKey('location.Id'))
+    originid = db.Column(db.Integer, ForeignKey('location.Id'))
+    arrivaltime = db.Column(db.Time)
+    departuretime = db.Column(db.Time)
+    amountofpassengers = db.Column(db.Integer)
+    boardinggateid = db.Column(db.Integer, ForeignKey('boardinggates.id'))
+    flightstatusid = db.Column(db.Integer, ForeignKey('flightstatus.id'))
+
+    def __init__(self, id, name, destinationid, arrivaltime, departuretime, amountofpassengers, boardinggateid, flightstatusid):
+        self.id = id
+        self.destinationid = destinationid
+        self.arrivaltime = arrivaltime
+        self.departuretime = departuretime
+        self.amountofpassengers = amountofpassengers
+        self.boardinggateid = boardinggateid
+        self.flightstatusid = flightstatusid
+
+    def __repr__(self):
+        return {
+            "id": self.id, 
+            "destinationid": self.destinationid,
+            "originid" : self.originid,
+            "arrivaltime" : self.arrivaltime,
+            "departuretime" : self.departuretime,
+            "amountofpassengers" : self.amountofpassengers,
+            "boardinggateid": self.boardinggateid,
+            "flightstatusid": self.flightstatusid
+        }
+
+
+class LocationModel(db.Model):
+
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(), nullable=False)
+    country = db.Column(db.String(), nullable=False)
+
+    def init(self, id, city, country):
+        self.id = id
+        self.city = city
+        self.country = country
+
+    def str(self):
+        return "Location {self.id}: {self.city} {self.country}"
 
 @app.route('/')
 def index():
     return {
         "message" : "hello"
     }
-
 
 
 @app.route('/flight', methods=['GET', 'POST'])
@@ -44,3 +107,66 @@ def function():
         results = [{"id": fl.id,"name": fl.name}for fl in flightStatus ]
 
         return {"flights": results}
+
+
+@app.route('/all_flights', methods=['GET'])
+def getAllFlights():
+    if request.method == 'GET':
+        flights = FlightsModel.query.all()
+        results = [{
+            "id": flight.id,
+            "desinationId": flight.destinationid,
+            "originId" : flight.originid,
+            "arrivalTime" : flight.arrivaltime,
+            "departureTime": flight.departuretime,
+            "amountofpassengers": flight.amountofpassengers,
+            "boardingGateId": flight.boardinggateid,
+            "flightStatusId": flight.flightstatusid
+        }for flight in flights]
+
+        return {"all flights": results}
+
+
+@app.route('/locations', methods=['GET'])
+def getAllLocations():
+    if request.method == 'GET':
+        locations = LocationModel.query.all()
+        results = [{
+            "id": location.id,
+            "city": location.city,
+            "country": location.country
+        }for location in locations]
+
+        return {"all locations": results}
+
+@app.route('/flight/<flight_id>', methods=['DELETE'])
+def deleteFlight(flight_id):
+    flight = FlightsModel.query.get(flight_id).delete()
+    
+    db.session.commit()
+
+    return {"message": "flight {flight_id} deleted"}
+
+@app.route('/flight/<flight_id>', methods=['PUT'])
+def updateFlight(flight_id):
+
+    req = request.get_json()
+
+    flight = FlightsModel.query.get(flight_id).delete()
+    
+    if flight is None:
+        return {"message": "Flight does not exists"}, 404
+
+
+    json = request.get_json(force=True)
+
+    flight.destinationid = req.destinationid
+    flight.originId = req.originId
+    flight.arrivalTime = req.arrivalTime
+    flight.departureTime = req.departureTime
+    flight.amountofpassengers = req.amountofpassengers
+    flight.boardingGateId = req.boardingGateId
+
+    flight.update()
+
+    return {"flight": flight.json()}
